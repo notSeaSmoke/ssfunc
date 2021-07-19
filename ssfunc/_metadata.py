3682c2e5d97c618c6d9d664ec859bf7c238ad3e6
NotSeaSmoke <notseasmoke@gmail.com>
2021-07-19 20:21:36 +0530

    Bump version number
    Also update required python version.

--- ssfunc/_metadata.py
+++ ssfunc/_metadata.py
@@ -1,4 +1,4 @@
 __author__ = "SeaSmoke (@SeaSmoke#0002)"
-__version__ = "0.0.1"
+__version__ = "0.0.2"
 __description__ = "SeaSmoke's VapourSynth functions. Varying levels of useless."
-__python__ = ">=3.8"
+__python__ = ">=3.9"
